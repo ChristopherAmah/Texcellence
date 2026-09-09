d93c2a4c589559fdcdf6acd4f590ed25eb2f997c
Christopher Amah <christopheramah0@gmail.com>
2026-09-09 16:11:56 +0100

    user interface

--- src/components/layouts/AppShell.jsx
+++ src/components/layouts/AppShell.jsx
@@ -1,7 +1,8 @@
 import { LogOut } from 'lucide-react';
+import logo from '../../assets/texcellence_logo.png';
 
 function AppShell({ user, logout, children }) {
-  return <main className="app-shell dashboard-shell"><header className="topbar"><span className="wordmark dark-mark">TEX<span>CELLENCE</span></span><div className="user-menu"><span>{user.firstName} {user.lastName}</span><button className="icon-button" onClick={logout} title="Sign out" aria-label="Sign out"><LogOut size={18} /></button></div></header>{children}</main>;
+  return <main className="app-shell dashboard-shell"><header className="topbar"><img className="app-logo" src={logo} alt="The TeXcellence Conference" /><div className="user-menu"><span>{user.firstName} {user.lastName}</span><button className="icon-button" onClick={logout} title="Sign out" aria-label="Sign out"><LogOut size={18} /></button></div></header>{children}</main>;
 }
 
 export default AppShell;
